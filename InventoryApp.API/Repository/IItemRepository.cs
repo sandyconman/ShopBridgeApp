54db100965ddea9acfed352e7558850daefa5aa3
Sandeep Kumar (HCL America Inc) <v-sandku@microsoft.com>
2020-08-24 14:09:19 +0530

    IItem repository comments

--- InventoryApp.API/Repository/IItemRepository.cs
+++ InventoryApp.API/Repository/IItemRepository.cs
@@ -6,10 +6,19 @@ namespace InventoryApp.API.Repository
 {
     public interface IItemRepository
     {
+        // Add a record to an entity T
         void Add<T>(T entity) where T: class;
+
+        // delete a record from an entry
         void Delete<T>(T entity) where T: class;
+
+        // commit changes to database
         Task<bool> SaveAll();
+
+        // Get all items from inventory
         Task<IEnumerable<Item>> GetAllItems();
+
+        // get an item from inventory
         Task<Item> GetItem(int id);
 
     }
